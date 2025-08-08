2ec8d714605b3890548620f68f6312c599d3b55e
sufyanmalik69 <sufyanmaliksaifi@gmail.com>
2025-08-08 18:23:39 +0500

    Updated settings.py

--- Blogsters/settings.py
+++ Blogsters/settings.py
@@ -29,6 +29,10 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS',default='127.0.0.1,localhost',cast=Csv())
 
+import os
+
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', cast=lambda v: [s.strip() for s in v.split(',')])
+
 
 # Application definition
 
