308ec6fa18a84e5d764bd7e68e357562e4f12946
SufyanMalikT <sufyanmaliksaifi@gmail.com>
2026-02-14 13:48:51 +0500

    minor changes

--- Blogsters/wsgi.py
+++ Blogsters/wsgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+import os
+import sys
+
+# 1. Add your project directory to the sys.path
+path = '/home/sufyanmalik123/Blogsters'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+# 2. Tell Django where your settings are
+# Make sure 'Blogsters' matches the folder name containing your settings.py
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Blogsters.settings'
+
+# 3. Import the Django WSGI handler
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Blogsters.settings')
-
 application = get_wsgi_application()
--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn Blogsters.wsgi:application --bind 0.0.0.0:$PORT
